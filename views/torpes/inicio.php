002fdbc9058775a0465f7f687336a25541b63144
cares <soycares@gmail.com>
2017-10-04 13:18:23 +0200

    quitar contador de tiempo

--- views/torpes/inicio.php
+++ views/torpes/inicio.php
@@ -28,6 +28,7 @@
 					// Tiempo hasta cerrar jornada
 					?>
 					<!-- INICIO -->
+					<!--
 					<div class="row">
 						<div class="col-sm-12 col-md-12 col-xs-12 text-center">
 							<link rel="stylesheet" href="<?=base_url()?>/css/jquery.countdown.css">
@@ -57,6 +58,7 @@
 							</div>
 						</div>
 					</div>
+					-->
 					<!-- FIN -->
 					<?php
 					if ($rs->num_rows() > 0) {
